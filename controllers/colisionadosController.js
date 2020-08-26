b42fb324c08c635d19e5a15c29996cb5f067b525
maristin <xamarbape@gmail.com>
2020-08-25 19:53:29 -0500

    xm

--- controllers/colisionadosController.js
+++ controllers/colisionadosController.js
@@ -24,4 +24,86 @@ controllers.registroAfectado = async(req,res) =>{
 };
 
 
+controllers.registroTrasladoHospital = async(req,res) =>{
+    const TrasladoHospital = {
+        idAfectado : req.body.idAfectado,
+        nombreHospital : req.body.nombreHospital,
+        paseMedico : req.body.paseMedico,
+    };
+
+    TrasladoHospital.create(TrasladoHospital)
+    .then(TrasladoH=>{
+        res.json({success:true, data: TrasladoH});
+    })
+    .catch(err =>{
+        console.log("ERROR >:",err);
+        res.json({success:false, message: err});
+    })
+};
+
+controllers.registroDatosSeguro = async(req,res) =>{
+    const DatosSeguro = {
+        horaArribo : req.body.horaArribo,
+        tiempoRespuesta : req.body.tiempoRespuesta,
+        seguro : req.body.seguro,
+        corresponde : req.body.corresponde,
+        nombreAjustador : req.body.nombreAjustador,
+        unidadSeguro : req.body.unidadSeguro,
+        
+    };
+
+    DatosSeguro.create(DatosSeguro)
+    .then(DatosSegur=>{
+        res.json({success:true, data: DatosSegur});
+    })
+    .catch(err =>{
+        console.log("ERROR >:",err);
+        res.json({success:false, message: err});
+    })
+};
+
+controllers.registroEvento = async(req,res) =>{
+    const Evento = {
+        fecha : req.body.fecha,
+        hora : req.body.hora,
+        tipo_incidente : req.body.tipo_incidente,
+        descripción : req.body.descripción,
+        tramo : req.body.tramo,
+        idOperador : req.body.idOperador,
+        folioBitacora : req.body.folioBitacora,
+        
+    };
+
+    Evento.create(Evento)
+    .then(Event=>{
+        res.json({success:true, data: Event});
+    })
+    .catch(err =>{
+        console.log("ERROR >:",err);
+        res.json({success:false, message: err});
+    })
+};
+
+controllers.registroDatosAmbulancia = async(req,res) =>{
+    const DatosAmbulancia = {
+        tiempoLlegada : req.body.tiempoLlegada,
+        tiempoRespuesta : req.body.tiempoRespuesta,
+        ambulancia : req.body.ambulancia,
+        ecoPlaca : req.body.ecoPlaca,
+        paramedico : req.body.paramedico,
+        diagnostico : req.body.diagnostico,
+        
+    };
+
+    DatosAmbulancia.create(DatosAmbulancia)
+    .then(DatosAmbulanci=>{
+        res.json({success:true, data: DatosAmbulanci});
+    })
+    .catch(err =>{
+        console.log("ERROR >:",err);
+        res.json({success:false, message: err});
+    })
+};
+
+
 module.exports = controllers;
--- routes/colisionadosRoutes.js
+++ routes/colisionadosRoutes.js
@@ -4,7 +4,12 @@ const router = express.Router();
 
 const colisionadosControll = require('../controllers/colisionadosController');
 
+
 router.post("/registro-afectado",colisionadosControll.registroAfectado);
+router.post("/registro-TrasladoHospital",colisionadosControll.registroTrasladoHospital);
+router.post("/registro-DatosSeguro",colisionadosControll.registroDatosSeguro);
+router.post("/registro-Evento",colisionadosControll.registroEvento);
+router.post("/registro-DatosAmbulancia",colisionadosControll.DatosAmbulancia);
 
 
 module.exports = router;
