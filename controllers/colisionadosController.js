75395245a8584078d798089ef4238e68471e412c
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-09-05 19:35:57 -0500

    Modelos de lesiones, routeres y controladores

---
+++ controllers/colisionadosController.js
@@ -0,0 +1,145 @@
+const db = require('../config/db');
+const Colision = require('../models/Colisiones/Colision');
+const DatosSeguroColision = require('../models/Colisiones/DatosSeguroColision');
+const Lesionado = require('../models/Colisiones/Lesionado');
+const DatosAutomovil = require('../models/Colisiones/DatosAutomovil');
+const controllers = {};
+
+//Borra los datos y tablas al correr el server siempre y caundo sync este en true
+db.sync({force:false});
+
+// POST
+controllers.addColision = async(req,res)=>{
+    const _colision = {
+        sentido: req.params.sentido,
+        motivo: req.params.motivo,
+        interseccion: req.params.interseccion,
+        colonia : req.params.colonia,
+        fecha:req.params.fecha,
+        hora: req.params.hora 
+    };
+
+    Colision.create(_colision)
+    .then(col=>{
+        res.json({ success: true, data: col });
+    })
+    .catch(err=>{
+        console.log("ERROR >:", err);
+            res.json({ success: false, message: err });
+    });
+};
+
+
+controllers.addSeguro = async(req,res)=>{
+    const _colision = await Colision.findOne({where:{id:req.params.idColision}});
+    const _datosSeguro = {
+        nombre_seguro: req.params.nombre_seguro,
+        tipo_seguro: req.params.tipo_seguro,
+        paga: req.params.paga,
+        comentarios : req.params.comentarios,
+    };
+
+    DatosSeguroColision.create(_datosSeguro)
+    .then(obj=>{
+        obj.addColisions(_colision);
+        res.json({ success: true, data: obj });
+    })
+    .catch(err=>{
+        console.log("ERROR >:", err);
+            res.json({ success: false, message: err });
+    });
+};
+
+controllers.addLesionado = async(req,res)=>{    
+    const _datosLesionado = {
+        sexo: req.params.sexo,
+        tipo_lesionado: req.params.tipo_lesionado,     
+        fk_colision: req.params.idColision,
+    };
+
+    Lesionado.create(_datosLesionado)
+    .then(obj=>{        
+        res.json({ success: true, data: obj });
+    })
+    .catch(err=>{
+        console.log("ERROR >:", err);
+            res.json({ success: false, message: err });
+    });
+};
+
+
+controllers.addAutomovil = async(req,res)=>{    
+    const _datosAutomovil = {
+        sexo_contuctor: req.params.sexo,
+        marca: req.params.tipo_lesionado,
+        submarca:req.params.submarca,
+        color: req.params.color,
+        placa: req.params.placa,     
+        fk_colision: req.params.idColision
+    };
+
+    DatosAutomovil.create(_datosAutomovil)
+    .then(obj=>{        
+        res.json({ success: true, data: obj });
+    })
+    .catch(err=>{
+        console.log("ERROR >:", err);
+            res.json({ success: false, message: err });
+    });
+};
+
+
+// GET
+controllers.getColisiones = async (req,res)=>{
+    await Colision.findAll()
+    .then(obj=>{
+        res.json({success:true, data:obj});
+    })
+    .catch(err=>{
+        res.json({success:false, message:err});
+    })
+}
+
+
+controllers.getAutomovil = async (req,res) =>{
+    await DatosAutomovil.findAll({where:{
+        fk_colision: req.params.idColision
+     }})
+     .then(obj=>{
+         res.json({success:true, data:obj});
+     })
+     .catch(err=>{
+         res.json({success:false, message:err});
+     })
+};
+
+controllers.getLesionados = async (req,res) =>{
+    await Lesionado.findAll({where:{
+        fk_colision: req.params.idColision
+     }})
+     .then(obj=>{
+         res.json({success:true, data:obj});
+     })
+     .catch(err=>{
+         res.json({success:false, message:err});
+     })
+};
+
+// DELETE
+
+controllers.deleteColision = async (req,res)=>{
+    const id_colision = req.params.idColision;
+    await Colision.destroy({ 
+        where : {
+            id: id_colision
+        }
+    })
+    .then(()=>{
+        res.json({success:true});
+    })
+    .catch(err=>{
+        res.json({success:false, message:err});
+    })
+}
+
+module.exports = controllers;
---
+++ routes/colisionadosRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+// controller
+const controllers = require('../controllers/colisionadosController');
+
+// POST
+router.post('/datos-automovil/:idColision',controllers.addAutomovil);
+router.post('/datos-seguro/:idColision',controllers.addSeguro);
+router.post('/datos-lesionado/:idColision',controllers.addLesionado);
+router.post('/datos-colision',controllers.addColision);
+// GET
+router.get('/colisiones-list',controllers.getColisiones),
+router.get('/automovil-list/:idColision',controllers.getAutomovil);
+router.get('/seguro-list/:idColision',controllers.getSeguro);
+router.get('/lesionados-list/:idColision',controllers.getLesionados);
+// DELETE
+router.delete('/delete-colision/:idColision',controllers.deleteColision);
+
+
+module.exports= router();
