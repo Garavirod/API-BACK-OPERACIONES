b2c6b98734357bbc053b8f8a95248078131e5ae3
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-11-05 13:40:24 -0600

    Se agrgó la empresa a la consulta

--- controllers/desincorporacionController.js
+++ controllers/desincorporacionController.js
@@ -450,7 +450,7 @@ controllers.getFoliosDataBrief = async (req, res)=>{
             break;
     }    
     await Desincorporacion.findAll({
-        attributes:["id","fecha","hora","motivo","jornada","estacion","linea","observaciones","tipo"],
+        attributes:["id","fecha","hora","empresa","motivo","jornada","estacion","linea","observaciones","tipo"],
         include:[ 
             {   
                 model:Cumplimiento_incumplimiento,
